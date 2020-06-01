a702e909011d33d2b650be660aeff441c8838419
ozarkk191 <ozarkk19@gmail.com>
2020-06-02 06:00:00 +0700

    move loading state to be a global state

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -13,90 +13,98 @@ class AuthenticatePage extends StatefulWidget {
 }
 
 class _AuthenticatePageState extends State<AuthenticatePage> {
+  bool loadingState = false;
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    final AuthService _auth = AuthService();
-    bool loadingState = false;
-
-    return loadingState
-        ? Loading()
-        : MaterialApp(
-            home: Scaffold(
-              body: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("images/splash_screen.jpg"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      ClipOval(
-                        child: Container(
-                          width: 150.0,
-                          height: 150.0,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image:
-                                  AssetImage("images/facebook_login_btn.png"),
-                            ),
-                          ),
-                          child: FlatButton(
-                            padding: EdgeInsets.all(0.0),
-                            onPressed: () async {
-                              setState(() {
-                                loadingState = true;
-                              });
-                              dynamic result = await _auth.loginWithFB();
-                              if (result == null) {
-                                setState(() {
-                                  loadingState = false;
-                                });
-
-                                print('null result from facebook.');
-                              } else {
-                                setState(() {
-                                  loadingState = false;
-                                });
-
-                                print('>\n>\n>\n');
-                                print('signed in');
-                                print(result.uid);
-                              }
-                            },
-                            child: null,
-                          ),
-                        ),
-                      ),
-                      RaisedButton(
-                        child: Text('Skip log-in'),
-                        onPressed: () {
-                          setState(() {
-                            loadingState = true;
-                            print(
-                                '------------------------------> loadingState: $loadingState');
-                          });
-                        },
-                        // onPressed: () async {
-                        //   dynamic result = await _auth.signInAnon();
-                        //   if (result == null) {
-                        //     print('error signing in');
-                        //   } else {
-                        //     print('>\n>\n>\n');
-                        //     print('signed in');
-                        //     print(result.uid);
-                        //   }
-                        // },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/splash_screen.jpg"),
+              fit: BoxFit.cover,
             ),
-          );
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                loadingState
+                    ? CircularProgressIndicator()
+                    : Container(
+                        child: Text('loading State: $loadingState'),
+                      ),
+                ClipOval(
+                  child: Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/facebook_login_btn.png"),
+                      ),
+                    ),
+                    child: FlatButton(
+                      padding: EdgeInsets.all(0.0),
+                      onPressed: () async {
+                        setState(() {
+                          loadingState = true;
+                          print('loading------ $loadingState');
+                        });
 
-    throw UnimplementedError();
+                        dynamic result = await _auth.loginWithFB();
+                        if (result == null) {
+                          if (this.mounted) {
+                            setState(() {
+                              loadingState = true;
+                            });
+                          }
+
+                          print('null result from facebook.');
+                        } else {
+                          if (this.mounted) {
+                            setState(() {
+                              loadingState = true;
+                            });
+                          }
+
+                          print('>\n>\n>\n');
+                          print('signed in');
+                          print(result.uid);
+                        }
+                      },
+                      child: Container(),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  child: Text('Skip log-in'),
+                  onPressed: () {
+                    setState(() {
+                      loadingState = true;
+                      print(
+                          '------------------------------> loadingState: $loadingState');
+                    });
+                  },
+                  // onPressed: () async {
+                  //   dynamic result = await _auth.signInAnon();
+                  //   if (result == null) {
+                  //     print('error signing in');
+                  //   } else {
+                  //     print('>\n>\n>\n');
+                  //     print('signed in');
+                  //     print(result.uid);
+                  //   }
+                  // },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // throw UnimplementedError();
   }
 }
